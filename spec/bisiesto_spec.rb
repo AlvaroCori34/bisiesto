dc1e2e4edf08c1455c9f3d82885ec82e7aca31db
AlvaroCori <bacs200018@gmail.com>
2021-04-24 21:37:43 -0400

    segunda prueba anio divisible entre 4

--- spec/bisiesto_spec.rb
+++ spec/bisiesto_spec.rb
@@ -2,8 +2,11 @@ RSpec.describe "bisiesto" do
     it "Para el año 4 deberia devolver true" do
         expect(es_bisiesto(4)).to eq(true)
     end
+    it "Para el año 5 deberia devolver false" do
+        expect(es_bisiesto(5)).to eq(false)
+    end
 end
 
 def es_bisiesto(anio)
-    true
+    return anio%4==0
 end
